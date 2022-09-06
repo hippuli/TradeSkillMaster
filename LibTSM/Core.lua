e3d59d6006b8ef3e4cb3ede653e8634d58121fac
Konsta Niinimaki <konsta.niinimaki@servur.org>
2022-09-06 11:29:22 +0000

    Update

--- LibTSM/Core.lua
+++ LibTSM/Core.lua
@@ -114,7 +114,7 @@ function TSM.ModuleInfoIterator()
 end
 
 function TSM.IsWowClassic()
-	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC
 end
 
 function TSM.IsWowVanillaClassic()
@@ -122,12 +122,7 @@ function TSM.IsWowVanillaClassic()
 end
 
 function TSM.IsWowWrathClassic()
-	if WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
-		assert(LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_WRATH_OF_THE_LICH_KING)
-		return true
-	else
-		return false
-	end
+	return WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
 end
 
 function TSM.DebugLogout()
